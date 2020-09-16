3d0165364d3c53ad075fa17d748641d74e188a4c
Roman <gavrin.rom@gmail.com>
2020-09-16 20:59:28 +0300

    Завершение и правильное решение 6 задания

--- stage-6.py
+++ stage-6.py
@@ -1,12 +1,17 @@
+def protect(s):
+    lower = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    return lower.intersection(s.lower()) != set()
+
+
 def my_func(text):
-    return text[0].title() + text[1:]
+    return text.title()
 
 
 while True:
     a = input('Введите строку : ').split()
     for el in a:
-        if not el.isalpha():
-            print('Ошибка формат ввода данных')
+        if protect(el):
+            print('Неправильна была введена строка!!')
             break
     else:
         b = ''
